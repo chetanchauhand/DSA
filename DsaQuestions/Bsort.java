14301955729a5c638124d5a765f7ce2cf0a33637
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-10 19:21:22 +0530

    1st

--- DsaQuestions/Bsort.java
+++ DsaQuestions/Bsort.java
@@ -11,7 +11,7 @@ public class Bsort {
         int arr[] = {12,11,3,2,4} ;
         for (int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j+1];
                     arr[j] = arr[j];
                     arr[j+1] = temp;
